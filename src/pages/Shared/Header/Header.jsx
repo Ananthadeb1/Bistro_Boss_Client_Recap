f16f8c3922204edb9e7538cf77b19e284bf5fc9a
Anantha Debnath <ananthadebnath103@gmail.com>
2025-03-06 14:46:32 +0600

    fixed navbar and carousol done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 
     return (
         <>
-            <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar fixed z-10  bg-black/40 mx-auto max-w-screen-xl  text-white  ">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
